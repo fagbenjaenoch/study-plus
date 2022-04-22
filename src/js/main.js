8b925b8d4aa7d9ebe22c914303767410ea2bbbaa
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2022-04-22 11:01:45 +0100

    Added sticky header feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-const 
+window.onscroll = function(){stick()};
+
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function stick() {
+    if (window.pageYOffset > sticky){
+        header.classList.add("sticky");
+    }
+
+    else{
+        header.classList.remove("sticky");
+    }
+}
